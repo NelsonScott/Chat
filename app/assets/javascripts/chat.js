4fa290465333298189ad43abc26db8a877978b15
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-03-10 18:44:04 -0700

    add js directories for backbone

---
+++ app/assets/javascripts/chat.js
@@ -0,0 +1,18 @@
+window.Chat = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    var $chatBox = $('#chat-box');
+    var $rooms = $('#rooms');
+
+    // var roomsView = new Chat.Views.rooms({
+    //   collection:
+    // });
+    // $rooms.html(roomsView.render().$el);
+
+    // new Redly.Routers.Router($chatBox);
+    // Backbone.history.start();
+  }
+}
