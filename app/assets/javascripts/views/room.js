e2614cf90a72c6a99fe8ee0bf665aedfd139f115
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-03-22 17:23:54 -0700

    add bootply styling to rooms list, add header

--- app/assets/javascripts/views/room.js
+++ app/assets/javascripts/views/room.js
@@ -7,6 +7,19 @@ Chat.Views.room = Backbone.View.extend({
     this.room = options.room;
   },
 
+  events: {
+    "mouseenter": "activate",
+    "mouseleave": "deactivate",
+  },
+
+  activate: function(){
+    this.$(".list-group-item").addClass("active");
+  },
+
+  deactivate: function(){
+    this.$(".list-group-item").removeClass("active");
+  },
+
   render: function(){
     var content = this.template({room: this.room});
     this.$el.html(content);
